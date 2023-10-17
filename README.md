cb94a4e9c9f14ac800d4ca2d750963a9fa11fc1b
unknown <zhixian2272@gmail.com>
2023-10-17 18:34:33 +0800

    Version Submit

---
+++ README.md
@@ -0,0 +1,38 @@
+## Personal Info
+何芷倩 112598401 資訊所碩一
+
+## PC Spec
+1) Processor: Intel(R) Core(TM) i7-10700 CPU @ 2.90GHz   2.90 GHz
+2) Installed RAM: 48.0GB
+
+## Versions:
+1) Spark: 3.2.4
+2) Scala: 2.13.5
+3) sbt: 1.9.6
+4) Java: 1.8.0_202 (JDK 8u202)
+
+## Clarification
+This homework is runned in Visual Studio Code with sbt build tool. I have allocated 2GB for the maximum heap size of JVM for better peroformance using '-J-Xmx2G' in *.sbtopt*.
+
+To run this project:
+`$sbt run`
+
+Please place the input file at the outermost layer of the project's directory.
+After running the project, 2 folders named "target" will be produced.
+The output of sub-task 3 is named "output.txt"
+```
+├─project
+│  └─build.properties
+│  └─*target*
+├─src
+│  └─main
+│      └─scala
+├─*target*
+├─.sbtopts
+├─build.sbt
+├─household_power_consumption.txt
+├─output.txt
+```
+
+## Generated Output
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,9 +2,16 @@ import org.apache.spark.sql.{SparkSession, Row, types => T}
 import org.apache.log4j.{Logger, Level}
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
-import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
+import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.ml.feature.{MinMaxScaler, VectorAssembler}
+import org.apache.spark.ml.linalg.DenseVector
+
+import java.io.{BufferedWriter, FileWriter, File}
 
 import T.DoubleType
+import T.LongType
+import T.StructType
+import T.StringType
 
 object Main extends App {
   Logger.getLogger("org").setLevel(Level.FATAL)
@@ -15,64 +22,74 @@ object Main extends App {
     .master("local[2]") //run locally with 2 cores
     .getOrCreate()
 
-  // val data = List("Hello, world", "I'm running Spark!")
-  // val msgDF = spark.createDataFrame(
-  //   spark.sparkContext.makeRDD(data.map(x => Row(x))),
-  //   schema = T.StructType(Array(T.StructField("msg", T.StringType)))
-  // )
-  // msgDF.foreach((row: Row) => println(row.getAs("msg")))
-
   val dataPath = "household_power_consumption.txt" // Replace with your actual file path
-    val rawData: DataFrame = spark.read
-      .option("delimiter", ";")
-      .option("header", "true")
-      .csv(dataPath)
+  val rawData: DataFrame = spark.read
+    .option("delimiter", ";")
+    .option("header", "true")
+    .csv(dataPath)
 
-      
-    val castedDF = rawData
-      .withColumn("Global_active_power", col("Global_active_power").cast(DoubleType))
-      .withColumn("Global_reactive_power", col("Global_reactive_power").cast(DoubleType))
-      .withColumn("Voltage", col("Voltage").cast(DoubleType))
-      .withColumn("Global_intensity", col("Global_intensity").cast(DoubleType))
-      .na.fill(0)
+  val columnsToAnalyze = Seq("Global_active_power","Global_reactive_power","Voltage","Global_intensity")
 
+  val selectedDF = rawData.select(columnsToAnalyze.map(col): _*)
+  val processedDF = selectedDF.select(selectedDF.columns.map(col(_).cast(DoubleType)): _*).na.fill(0)
 
-    val columnsToAnalyze = Seq("Global_active_power","Global_reactive_power","Voltage","Global_intensity")
-    
-    // Task 1: Output the minimum, maximum, and count of specific columns
-    val minMaxCountData = calculateMinMaxCount(castedDF, columnsToAnalyze)
-    minMaxCountData.show()
+  val outputPath = "output.txt"
 
-    // // Task 2: Output the mean and standard deviation of specific columns
-    // val meanStdData = calculateMeanStdDev(castedDF, columnsToAnalyze)
-    // meanStdData.show()
-
-    // // Task 3: Perform min-max normalization on the specified columns
-    // val normalizedData = performMinMaxNormalization(castedDF, columnsToAnalyze)
-    // normalizedData.show()
-
-    spark.stop()
-
-  def calculateMinMaxCount(data: DataFrame, columns: Seq[String]): DataFrame = {
-    val selectExprs = columns.flatMap { colName =>
-      Seq(
-        expr(s"min($colName) as min_$colName"),
-        expr(s"max($colName) as max_$colName"),
-        expr(s"sum($colName) as sum_$colName")
-      )
-    }
-
-    data.select(selectExprs: _*)
+  val outputFile = new File(outputPath)
+  if (outputFile.exists()) {
+    outputFile.delete()
   }
 
-  // def calculateMeanStdDev(data: DataFrame, columns: Seq[String]): DataFrame = {
-  //   // mean and standard deviation
-    
-  //   val summary: MultivariateStatisticalSummary = Statistics.colStats(data)
-  // }
+  val fileWriter = new FileWriter(outputPath, true) // `true`: append mode
+  val bufferedWriter = new BufferedWriter(fileWriter)
 
-  // def performMinMaxNormalization(data: DataFrame, columns: Seq[String]): DataFrame = {
-  //   // Implement logic for min-max normalization
-  //   // Return a DataFrame with normalized output
-  // }
+  val listBuffer1: Seq[String] = performMinMaxNormalization(processedDF, columnsToAnalyze(0))
+  val listBuffer2: Seq[String] = performMinMaxNormalization(processedDF, columnsToAnalyze(1))
+  val listBuffer3: Seq[String] = performMinMaxNormalization(processedDF, columnsToAnalyze(2))
+  val listBuffer4: Seq[String] = performMinMaxNormalization(processedDF, columnsToAnalyze(3))
+
+  // sub-task 1&2
+  columnsToAnalyze.foreach(col => {
+    processedDF.describe(col).show()
+  })
+
+  val combinedList = listBuffer1 ++ listBuffer2 ++ listBuffer3 ++ listBuffer4
+
+  // Write output file
+  val dataCount = processedDF.count().toInt
+  val len = columnsToAnalyze.length-1
+  val range = dataCount-1
+  for(i <- 0 to range){
+    for(j <- 0 to len){
+      val index = i+(dataCount*j)
+      bufferedWriter.write(combinedList(index))
+      if(j<len)
+        bufferedWriter.write(",")
+      else
+        bufferedWriter.write("\n")
+    }
+  }
+
+  bufferedWriter.close()
+
+  spark.stop()
+
+  def performMinMaxNormalization (dataFrame: DataFrame, colName: String): Seq[String] = {
+    val assembler = new VectorAssembler()
+      .setInputCols(Array(colName))
+      .setOutputCol("vector_"+colName)
+
+    val assembledData = assembler.transform(dataFrame.select(colName))
+    val scaledColumn : String = "scaled_" + colName
+
+    val scaler = new MinMaxScaler()
+      .setInputCol("vector_" + colName)
+      .setOutputCol(scaledColumn)
+
+    val scaledData = scaler.fit(assembledData).transform(assembledData) //.show() //adding this cause error on using "select()" 
+    
+    val scaledDataSeq = scaledData.select(scaledColumn).collect().map(_.toString)
+
+    scaledDataSeq.map(value => value.substring(2, value.length - 2))
+  }
 }
