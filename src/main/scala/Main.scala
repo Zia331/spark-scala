5e0c78bcf947493195d77b0a0180c76cd1172718
zia <zhixian2272@gmail.com>
2023-12-28 18:13:34 +0800

    HW3: LSH map reduce

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{Logger, Level}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import org.apache.spark.rdd.RDD
 
 import scala.collection.mutable.Map
 import java.io.FileWriter
@@ -25,35 +26,37 @@ object Main extends App {
     // .master("spark://192.168.56.104:7077")
     // .config("spark.executor.memory", "30g")  // Set executor memory
     // .config("spark.driver.memory", "16g")  // Set driver memory
-    .master("local[8]") //run locally with 4 cores
+    .master("local[8]") //run locally
     .getOrCreate()
 
   val baseName = "reut2-"
   val fileExtension = ".sgm"
-  val numFiles = 3
+  val numFiles = 1
 
   // Generate file paths and store in an array
   val filenames = Array.tabulate(numFiles)(i => s"$baseName${"%03d".format(i)}$fileExtension")
 
-  val documents: List[String] = getBodyText(filenames)
+  val sc = spark.sparkContext
+  val documentsRDD: RDD[String] = sc.parallelize(getBodyText(filenames))
 
-  val k: Int = 2
+  val k: Int = 1
 
   // Initialize a mutable map to store the index of each shingle
   val shingleIndexMap: Map[String, Int] = Map.empty
 
   // Initialize a mutable list to store the binary representation of each document
-  val documentBinaryList: mutable.ListBuffer[DenseVector[Int]] = mutable.ListBuffer.empty
+  // val documentBinaryList: mutable.ListBuffer[DenseVector[Int]] = mutable.ListBuffer.empty
 
   // Initialize a set to store unique shingles across all documents
-  val shingleSet: Set[String] = generateKShingles(documents.toArray, k)
-
-  for ((shingle, index) <- shingleSet.zipWithIndex) {
+  val shingleSet: Set[String] = generateKShingles(documentsRDD.collect(), k)
+  println("shingleSet size:", shingleSet.size)
+  shingleSet.zipWithIndex.map{ case(shingle, index) => 
     shingleIndexMap.put(shingle, index)
   }
+  println("shingleIndexMap is out!")
 
   // Iterate over each document to generate shingles and construct the feature matrix
-  for ((document, docIndex) <- documents.zipWithIndex) {
+  val binaryVectorRDD: RDD[DenseVector[Int]] = documentsRDD.map { document =>
     val shingles = generateShingles(document, k)
 
     // Initialize a binary vector for the current document
@@ -64,74 +67,154 @@ object Main extends App {
       binaryVector(shingleIndexMap(shingle)) = 1
     }
 
-    // Add the binary vector to the list
-    documentBinaryList.append(binaryVector)
+    binaryVector
   }
 
-  // Convert the list of binary vectors to a dense matrix
-  val denseMatrixList: Seq[DenseMatrix[Int]] = documentBinaryList.map(_.toDenseMatrix)
-  val featureMatrix: DenseMatrix[Int] = DenseMatrix.vertcat(denseMatrixList: _*).t // Transpose the matrix
+  val denseMatrixRDD: RDD[DenseMatrix[Int]] = binaryVectorRDD.map(_.toDenseMatrix)
+  println("denseMatrix is out!")
 
+  // Transpose the matrices to have rows represent shingles and columns represent documents
+  // val denseMatrixRDD: RDD[DenseMatrix[Int]] = denseMatrixRDD.map(_.t)
+
+  // Concatenate the matrices
+  // val featureMatrix: DenseMatrix[Int] = DenseMatrix.vertcat(denseMatrixRDD.collect(): _*).t
+  // println("featureMatrix is out!")
   // Print the resulting feature matrix
-  println(featureMatrix)
-  val shingleArray = shingleSet.toArray
-  // shingleArray.foreach(println)
-  // println(shingleArray.size)
-  exportDenseMatrixToCsv(featureMatrix, shingleArray, "output1.csv")
+  // println(featureMatrix)
   
-/*
-  // val hashedValueList: mutable.ListBuffer[DenseVector[Int]] = mutable.ListBuffer.empty
-  // val numberHashFunctions = 5
-  // val hashFunctions = generateHashFunctions( numberHashFunctions, modulus)
-  // val modulus = shingleArray.size-1
-  // for( i <- 0 until featureMatrix.rows){
-  //   val hashedVal = hashFunctions.map( hashfunc => hashfunc(i))
-  //   val hashedVector = DenseVector.zeros[Int](numberHashFunctions)
-  //   for ((value, index) <- hashedVal.zipWithIndex){
-  //     hashedVector(index) = value
-  //   }
-  //   hashedValueList.append(hashedVector)
-  // }
+  // exportDenseMatrixToCsv(featureMatrix, shingleArray, "output1.csv")
+  // val featureMatrixRDD = sc.parallelize(featureMatrix.toArray.grouped(featureMatrix.cols).toArray.zip(shingleSet.toSeq))
 
-  // val hashedMatrixList: Seq[DenseMatrix[Int]] = hashedValueList.map(_.toDenseMatrix)
-  // val hashedMatrix: DenseMatrix[Int] = DenseMatrix.vertcat(hashedMatrixList: _*).t
-  // println(hashedMatrix)
-*/
+  // Use the exportDenseMatrixToCsv function
+  // exportDenseMatrixToCsv(featureMatrixRDD, "output1.csv")
+  // Combine the matrices into a single RDD
+  val combinedMatrixRDD: RDD[String] = denseMatrixRDD.flatMap { matrix =>
+    matrix.toArray.map(_.toString)
+  }
+
+  // Zip the combinedMatrixRDD with shingleSet
+  val numDocuments = denseMatrixRDD.count().toInt
+  println("NumDoc", numDocuments)
+  // val outputRDD: RDD[String] = combinedMatrixRDD.zipWithIndex.map { case (value, index) =>
+  //   val col = index % numDocuments
+  //   val row = index / numDocuments
+  //   // println(index, row, col, value)
+  //   s"${shingleSet.toArray.apply(row.toInt)},$col,$value"
+  // }
+  val outputRDD: RDD[String] = combinedMatrixRDD
+    .zipWithIndex
+    .groupBy { case (_, index) => index % numDocuments }
+    .map { case (_, values) =>
+      val firstElement = shingleSet.toArray.apply((values.head._2 / numDocuments).toInt)
+      val columnValues = values.map { case (_, value) => value }.mkString(", ")
+      s"$firstElement, $columnValues"
+  }
+  outputRDD.foreach(println)
+
+  // Save the outputRDD to a CSV file
+  // outputRDD.saveAsTextFile("output1")
 
   val numberHashFunctions = 5
-  val modulus = shingleArray.size-1
+  val modulus = shingleSet.size-1
   val hashFunctions = generateHashFunctions( numberHashFunctions, modulus)
-  val minHashedMatrix = DenseMatrix.fill[Int](numberHashFunctions,featureMatrix.cols)(Int.MaxValue)
-  for( i <- 0 until featureMatrix.rows){
-    val hashedVal = hashFunctions.map( hashfunc => hashfunc(i))
-    for( j <- 0 until featureMatrix.cols){
-      if(featureMatrix(i,j) == 1){
-        for ((value, index) <- hashedVal.zipWithIndex){
-          if(value < minHashedMatrix( index, j)){
-            minHashedMatrix( index, j) = value
+  
+  // val minHashedMatrix = DenseMatrix.fill[Int](numberHashFunctions,featureMatrix.cols)(Int.MaxValue)
+  
+  // for( i <- 0 until featureMatrix.rows){
+  //   val hashedVal = hashFunctions.map( hashfunc => hashfunc(i))
+  //   for( j <- 0 until featureMatrix.cols){
+  //     if(featureMatrix(i,j) == 1){
+  //       for ((value, index) <- hashedVal.zipWithIndex){
+  //         if(value < minHashedMatrix( index, j)){
+  //           minHashedMatrix( index, j) = value
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  // Initialize the minHashedMatrix RDD with Int.MaxValue
+  var minHashedMatrixRDD = sc.parallelize(Seq.fill(numberHashFunctions, denseMatrixRDD.count().toInt)(Int.MaxValue))
+
+  // Broadcast hashFunctions to all worker nodes
+  val broadcastHashFunctions = sc.broadcast(hashFunctions)
+  println("broadcastHashFunctions")
+  // Perform the computation using RDD transformations
+  denseMatrixRDD.zipWithIndex.foreach { case (matrix, i) =>
+    val hashedVal = broadcastHashFunctions.value.map(hashfunc => hashfunc(i.toInt))
+    println(i)
+    // Update minHashedMatrixRDD based on the condition
+    minHashedMatrixRDD = minHashedMatrixRDD.zipWithIndex.mapPartitions { iter =>
+      iter.map { case (row, j) =>
+        val newRow = if (matrix(0, j.toInt) == 1) {
+          hashedVal.zip(row).map { case (value, currentMin) =>
+            math.min(value, currentMin)
           }
+        } else {
+          row
         }
+        newRow
       }
     }
   }
+
+  // Save the minHashedMatrixRDD to a CSV file
+  val minHashedOutputRDD: RDD[String] = minHashedMatrixRDD.zipWithIndex.map { case (row, j) =>
+    val col = j.toInt
+    val values = row.map(_.toString).mkString(",")
+    s"$col,$values"
+  }
+  println("Minhashed")
+
+  minHashedOutputRDD.saveAsTextFile("output2")
   
-  val hashArray: Array[Int] = Array.range(1, numberHashFunctions+1)
-  val hashSeq: Seq[String] = hashArray.map( n => n.toString).toSeq
+  // val hashArray: Array[Int] = Array.range(1, numberHashFunctions+1)
+  // val hashSeq: Seq[String] = hashArray.map( n => n.toString).toSeq
   // exportDenseMatrixToCsv(minHashedMatrix, hashSeq, "output2.csv")
-  println(minHashedMatrix)
-  println(s"Rows: ${minHashedMatrix.rows}, Cols: ${minHashedMatrix.cols}")
+  // println(minHashedMatrix)
+  // println(s"Rows: ${minHashedMatrix.rows}, Cols: ${minHashedMatrix.cols}")
 
   val numRowsPerBand = 200
-  val bands = minHashedMatrix.reshape(numRowsPerBand, minHashedMatrix.size/numRowsPerBand) //2d array: band*doc
-  val bandHashes = (0 until bands.rows).map(rowIdx => hashBand(bands(rowIdx, ::).t))
-  val bandGroups: Map[Int, Seq[Int]] =
-    mutable.Map() ++ bandHashes.zipWithIndex.toIndexedSeq
-    .groupBy { case (hash, docIndex) => hash }
-    .mapValues(_.map(_._2).toSeq)
+  val numCols = minHashedMatrixRDD.count().toInt
+  val bands = minHashedMatrixRDD
+    .zipWithIndex()
+    .map { case (row, idx) =>
+      (idx / numRowsPerBand, (idx % numRowsPerBand, row))
+    }
+    .groupByKey()
+    .sortByKey()
+    .map { case (_, indexedRows) =>
+      indexedRows.toSeq.sortBy(_._1).map(_._2)
+    }
 
-  // bandGroups.foreach { case (hash, docIndices) =>
-  //   println(s"Hash: $hash, Document Id (${docIndices.size}): ${docIndices.map(_.toInt + 1).mkString(", ")}")
-  // }
+  // val bands = minHashedMatrixRDD.reshape(numRowsPerBand, numCols/numRowsPerBand) //2d array: band*doc
+  // val bandHashes = (0 until bands.rows).map(rowIdx => hashBand(bands(rowIdx, ::).t))
+  // val bandGroups: Map[Int, Seq[Int]] =
+  //   mutable.Map() ++ bandHashes.zipWithIndex.toIndexedSeq
+  //   .groupBy { case (hash, docIndex) => hash }
+  //   .mapValues(_.map(_._2).toSeq)
+
+  // // bandGroups.foreach { case (hash, docIndices) =>
+  // //   println(s"Hash: $hash, Document Id (${docIndices.size}): ${docIndices.map(_.toInt + 1).mkString(", ")}")
+  // // }
+
+  // Use the hashBand function on each band to get bandHashes
+  // val bandHashes = bands.mapPartitions { bandIterator =>
+  //   bandIterator.map { band =>
+  //     val transposedBand = band.transpose
+  //     hashBand(transposedBand)
+  //   }
+  // }.collect()
+  val bandHashes = bands.mapPartitions { bandIterator =>
+    bandIterator.map(band => hashBand(DenseVector(band.flatten.toArray)))
+  }.collect()
+
+  // Create bandGroups using bandHashes and document indices
+  val bandGroups: Map[Int, Seq[Int]] = mutable.Map() ++
+    bandHashes.zipWithIndex.toIndexedSeq
+      .groupBy { case (hash, docIndex) => hash }
+      .mapValues(_.map(_._2).toSeq)
+
   val writer = new PrintWriter("output3.txt")
   try {
     for ((hash, docIndices) <- bandGroups) {
@@ -176,7 +259,8 @@ object Main extends App {
       try {
         val fileContent: String = Source.fromFile(path, "UTF-8").getLines().mkString("\n")
 
-        val ReutersPattern = """(?s)<REUTERS[^>]*LEWISSPLIT="TRAIN"[^>]*>(.*?)<\/REUTERS>""".r
+        // val ReutersPattern = """(?s)<REUTERS[^>]*LEWISSPLIT="TRAIN"[^>]*>(.*?)<\/REUTERS>""".r
+        val ReutersPattern = """(?s)<REUTERS[^>]*>(.*?)<\/REUTERS>""".r
         val BodyPattern = """(?s)<BODY>(.*?)<\/BODY>""".r
 
         val reutersMatches = ReutersPattern.findAllMatchIn(fileContent).map { reutersMatch =>
@@ -184,6 +268,8 @@ object Main extends App {
           val bodyContent = BodyPattern.findFirstMatchIn(reutersContent).map(_.group(1)).getOrElse("No BODY found")
           bodyContent
         }.toList
+
+        // val reutersMatches = BodyPattern.findAllMatchIn(fileContent).map(_.group(1)).toList
 
         // println(s"Number of Matches: ${reutersMatches.size}")
         reutersMatches
@@ -205,21 +291,38 @@ object Main extends App {
     input.split("\\s+").sliding(k).map(_.mkString(" ")).toSet
   }
 
-  def exportDenseMatrixToCsv(matrix: DenseMatrix[Int], index: Seq[String], filePath: String): Unit = {
-    val writer = new FileWriter(filePath)
-    val headers = (1 to matrix.cols).map(_.toString)
-    val csvPrinter = CSVFormat.DEFAULT.withHeader((Seq("") ++ headers): _*).print(writer)
+  // def exportDenseMatrixToCsv(matrix: DenseMatrix[Int], index: Seq[String], filePath: String): Unit = {
+  //   val writer = new FileWriter(filePath)
+  //   val headers = (1 to matrix.cols).map(_.toString)
+  //   val csvPrinter = CSVFormat.DEFAULT.withHeader((Seq("") ++ headers): _*).print(writer)
 
-    try {
-      val matrixArray = matrix.toArray.grouped(matrix.cols).toArray
-      for ((row, word) <- matrixArray.zip(index)) {
-        val values = row.map(_.toString)
-        csvPrinter.printRecord((word +: values): _*)
-      }
-    } finally {
-      writer.close()
-      csvPrinter.close()
-    }
-  }
+  //   try {
+  //     val matrixArray = matrix.toArray.grouped(matrix.cols).toArray
+  //     for ((row, word) <- matrixArray.zip(index)) {
+  //       val values = row.map(_.toString)
+  //       csvPrinter.printRecord((word +: values): _*)
+  //     }
+  //   } finally {
+  //     writer.close()
+  //     csvPrinter.close()
+  //   }
+  // }
+  // def exportDenseMatrixToCsv(matrixRDD: RDD[(Array[Int], String)], filePath: String): Unit = {
+  //   matrixRDD.foreachPartition { partition =>
+  //     println("Writing", filePath)
+  //     val writer = new FileWriter(filePath, true)  // Use "true" to append to the file
+  //     val csvPrinter = CSVFormat.DEFAULT.print(writer)
+
+  //     try {
+  //       for ((row, word) <- partition) {
+  //         val values = row.map(_.toString)
+  //         csvPrinter.printRecord((word +: values): _*)
+  //       }
+  //     } finally {
+  //       writer.close()
+  //       csvPrinter.close()
+  //     }
+  //   }
+  // }
   spark.stop()
 }
